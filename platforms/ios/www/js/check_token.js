5a581806ae04439abeb24e87c84faf80930596c9
Sankalp Singha <sankalpsingha@gmail.com>
2015-04-23 22:23:09 +0530

    Added the login section

--- platforms/ios/www/js/check_token.js
+++ platforms/ios/www/js/check_token.js
@@ -1,7 +1,9 @@
-$(document).ready(function(){
+// This is to check for the authentication values.
+// If it is present, then its ok, otherwise, just go to the login.
+function checkAuth() {
     console.log('xxxxxxxx-AuthenticationCheck-xxxxxxxxx');
     try {
-        if(localStorage.getItem('auth_token')) {
+        if (localStorage.getItem('auth_token')) {
             console.log('LS Auth found.');
             console.info('Authentication can continue.');
         } else {
@@ -10,9 +12,22 @@ $(document).ready(function(){
     } catch (exception) {
         next_page = 'login.html';
     } finally {
-        setTimeout(function(){
+        setTimeout(function () {
             //$('body').css('backgroundImage','none');
             window.location.href = next_page;
         }, 0);
     }
-});
+}
+
+// Lets add the ajax prefilter to the equation as well :
+
+function prefilter(){
+    $.ajaxPrefilter(function (options) {
+        if (!options.beforeSend) {
+            options.beforeSend = function (xhr) {
+                xhr.setRequestHeader('X-User-Token', localStorage.getItem('auth_token'));
+                xhr.setRequestHeader('X-User-Email', localStorage.getItem('username'));
+            }
+        }
+    });
+}
