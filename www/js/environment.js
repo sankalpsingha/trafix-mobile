073da307d22cfb32b10b26c6a8979c236e3eed45
Sankalp Singha <sankalpsingha@gmail.com>
2015-05-01 04:07:37 +0530

    Added all the other things

---
+++ www/js/environment.js
@@ -0,0 +1,8 @@
+// This is the environment JS file covering all the Settings.
+// You can keep adding here as we go along.
+host = "http://localhost:3000/";
+namespace = "api/v1/";
+loginUrl = host + "/token";
+
+apiUrl = host + namespace; // http://localhost:3000/api/v1/
+
